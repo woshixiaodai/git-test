d2ad80d560bd411a12d284dd75fdb1b6c897d9ca
DaiYuXun <1257783071@qq.com>
2022-06-04 14:35:37 +0800

    first commit

---
+++ src/main/java/com/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.java.git;
+
+/**
+ * @author dyx
+ * @create 2022-06-04 14:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
